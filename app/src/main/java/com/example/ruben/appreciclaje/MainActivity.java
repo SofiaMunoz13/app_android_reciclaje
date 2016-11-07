bf82ba31b17bae5e9b97c6fb9876001eb012db38
rubenVillca <ruben_wolf_9@hotmail.com>
2016-11-07 16:25:13 -0400

    Enlaces a las imagenes: ->vidrio ->plastico ->papel ->metal ->bateria ->goma

--- app/src/main/java/com/example/ruben/appreciclaje/MainActivity.java
+++ app/src/main/java/com/example/ruben/appreciclaje/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.ruben.appreciclaje;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
-    // Remove the below line after defining your own ad unit ID.
+public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setImageAction();
+    }
+    private void setImageAction() {
+        ImageView imgPapel = (ImageView) findViewById(R.id.imagePapel);
+        imgPapel.setOnClickListener(this);
+        ImageView imgVidrio = (ImageView) findViewById(R.id.imageVidrio);
+        imgVidrio.setOnClickListener(this);
+        ImageView imgPlastico = (ImageView) findViewById(R.id.imagePlastico);
+        imgPlastico.setOnClickListener(this);
+        ImageView imgMetal = (ImageView) findViewById(R.id.imageMetal);
+        imgMetal.setOnClickListener(this);
+        ImageView imgBateria = (ImageView) findViewById(R.id.imageBateria);
+        imgBateria.setOnClickListener(this);
+        ImageView imgGoma = (ImageView) findViewById(R.id.imageGoma);
+        imgGoma.setOnClickListener(this);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -25,5 +42,35 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onClick(View view) {
+        Intent activity;
+        switch(view.getId()) {
+            case R.id.imagePapel:
+                activity = new Intent(MainActivity.this, CartonActivity.class);
+                startActivity(activity);
+                break;
+            case R.id.imagePlastico:
+                activity = new Intent(MainActivity.this, PlasticoActivity.class);
+                startActivity(activity);
+                break;
+            case R.id.imageVidrio:
+                activity = new Intent(MainActivity.this, VidrioActivity.class);
+                startActivity(activity);
+                break;
+            case R.id.imageMetal:
+                activity = new Intent(MainActivity.this, MetalActivity.class);
+                startActivity(activity);
+                break;
+            case R.id.imageBateria:
+                activity = new Intent(MainActivity.this, BateriaActivity.class);
+                startActivity(activity);
+                break;
+            case R.id.imageGoma:
+                activity = new Intent(MainActivity.this, GomaActivity.class);
+                startActivity(activity);
+                break;
+        }
     }
 }
